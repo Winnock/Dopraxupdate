edf9266487483f88d6eccc327d3cd2026463db2b
Winnock <88110420+Winnock@users.noreply.github.com>
2023-03-23 22:20:38 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-git clone https://github.com/Winnock/IntimacySharingBot ok && cd ok && pip3 install -U -r requirements.txt
+git clone https://github.com/Winnock
+/
+File-Sharing-Bot ok && cd ok && pip3 install -U -r requirements.txt
 python3 main.py
