9ca759fe8bdf41235641253a7d87e562a4e34bbe
Winnock <88110420+Winnock@users.noreply.github.com>
2023-03-23 22:27:08 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-git clone https://github.com/Winnock
-/
-File-Sharing-Bot ok && cd ok && pip3 install -U -r requirements.txt
+git clone https://github.com/Winnock/File-Sharing-Bot ok && cd ok && pip3 install -U -r requirements.txt
 python3 main.py
